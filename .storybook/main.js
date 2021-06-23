4655eb815ca718a685a946b2d7fecbad9058a28c
S.Aksenchenko <stefanaksencenko@gmail.com>
2021-06-23 23:49:28 +0300

    On branch main 	Add settings to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../**/*.stories.mdx",
-    "../**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+          },
+        },
+        "sass-loader",
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
+};
