a37ebb41e0d909a9afccd0f885b727f70cb8165c
S.Aksenchenko <stefanaksencenko@gmail.com>
2021-06-24 00:27:18 +0300

    On branch main 	Add Headers

---
+++ src/Common/Headers/index.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { H1, H2, H3 } from "./index";
+export default {
+  component: H1,
+  title: "Common/Headers",
+};
+
+export const Header1 = () => <H1> Hello world </H1>;
+export const Header2 = () => <H2> Hello world </H2>;
+export const Header3 = () => <H3> Hello world </H3>;
---
+++ src/Common/Headers/index.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import styles from "./index.module.scss";
+import cn from "classnames";
+
+type Align = "left" | "center" | "right";
+
+export interface IProps {
+  children: ReactNode;
+  align: Align;
+}
+
+export const H1: React.FC<IProps> = ({ children, align = "left" }: IProps) => {
+  return (
+    <h1 className={cn(styles.hx, styles[`hx_align_${align}`], styles.h1)}>
+      {children}
+    </h1>
+  );
+};
+
+export const H2: React.FC<IProps> = ({ children, align = "left" }: IProps) => {
+  return (
+    <h2 className={cn(styles.hx, styles[`hx_align_${align}`], styles.h2)}>
+      {children}
+    </h2>
+  );
+};
+
+export const H3: React.FC<IProps> = ({ children, align = "left" }: IProps) => {
+  return (
+    <h3 className={cn(styles.hx, styles[`hx_align_${align}`], styles.h3)}>
+      {children}
+    </h3>
+  );
+};
