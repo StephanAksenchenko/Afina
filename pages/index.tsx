e624787d84af219b19210add6a2b8d62ae6f90e2
S.Aksenchenko <stefanaksencenko@gmail.com>
2021-06-25 01:32:23 +0300

    On branch main 	Main page fixies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import styles from '../styles/Home.module.css';
+import { Main } from "../src/Components/Layouts/Main";
+import { H1, H2 } from "../src/Components/Common/Headers";
+import { Flexbox } from "../src/Components/Common/Flexbox";
+import { ButtonLink } from "../src/Components/Common/Button";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}> Afina </h1>
-      <h2> Here will be modern, intelligent, full fantastic features platform for education </h2>
-    </div>
-  )
+    <Main>
+      <Flexbox justifyContent="center" alignItems="center">
+        <H1>Afina</H1>
+        <H2>
+          Here will be modern, intelligent, full fantastic features platform for
+          education
+        </H2>
+        <Flexbox flexDirection="row" style={{ marginTop: "48px" }}>
+          <ButtonLink href="/lessons"> Learn </ButtonLink>
+          <ButtonLink href="/login"> Login </ButtonLink>
+        </Flexbox>
+      </Flexbox>
+    </Main>
+  );
 }
