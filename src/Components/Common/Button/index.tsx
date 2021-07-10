1338cc544cec054b465c29417213c17807afffb8
S.Aksenchenko <stefanaksencenko@gmail.com>
2021-07-10 22:15:37 +0300

    On branch main 	Update button to add one more props classNames

--- src/Components/Common/Button/index.tsx
+++ src/Components/Common/Button/index.tsx
@@ -1,16 +1,22 @@
 import { ReactNode } from "react";
 import Link from "next/link";
 import styles from "./index.module.scss";
+import cn from "classnames";
 
 export interface ButtonLinkProps {
   children: ReactNode;
   href: string;
+  classNames?: string;
 }
 
-export const ButtonLink: React.FC<ButtonLinkProps> = ({ children, href }) => {
+export const ButtonLink: React.FC<ButtonLinkProps> = ({
+  children,
+  href,
+  classNames = "",
+}) => {
   return (
     <Link href={href}>
-      <a className={styles.buttonLink}>{children}</a>
+      <a className={cn(styles.buttonLink, classNames)}>{children}</a>
     </Link>
   );
 };
